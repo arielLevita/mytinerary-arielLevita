0078b766be88f761d3161a129e76f93f215f48cf
arielLevita <levita.ariel@gmail.com>
2023-08-08 01:04:02 -0300

    Responsive Navbar button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,31 @@
-import NavbarLinks from '../NavbarLinks/NavbarLinks';
+import { useState } from 'react';
+import NavbarMenu from '../NavbarMenu/NavbarMenu';
+import './Navbar.css'
 
 const Navbar = () => {
 
-  const links = [
-    {key: 1, title: 'Home', to: '/home'},
-    {key: 2, title: 'Cities', to: '/cities'},
-    {key: 3, title: 'Stays', to: '/stays'},
-    {key: 4, title: 'Car rentals', to: '/car-rentals'}
-  ]
+  let [show, setShow] = useState(false);
+
+  const handleShowMenu = () => {
+    setShow(!show)
+  }
 
   return (
-    <nav className="flex justify-between items-center bg-gradient-to-r from-blue-400 bg-cyan-700 p-2 w-full">
-      <div>
+    <nav className="navbar z-50 inline-flex bg-gradient-to-r from-blue-400 bg-cyan-700 p-2 w-full">
+      <div className='me-auto'>
         <img className='h-8 mx-4 aspect-square inline-block' src="https://i.postimg.cc/QMYfcPkT/mytinerary-logo.png" alt="logo" />
         <span className='text-white font-semibold'>MyTinerary</span>
       </div>
-      <div>
-        <div className='text-white inline-flex'>
-          {links.map((link) => (<NavbarLinks key={link.key} title={link.title} to={link.to}/>))}
-        </div>
-        <div className="inline-flex justify-between items-center">
-          <button className=" bg-gradient-to-r from-purple-500 bg-purple-400 mx-2 px-3 py-1 text-sm shadow-sm font-medium tracking-wider  text-white rounded-full hover:shadow-2xl hover:bg-purple-700 hover:from-purple-700">Login</button>
-          <img className='h-8 mx-2 aspect-square' src="https://i.postimg.cc/9MLsQhpR/blank-avatar.png" alt="blank avatar" />
-        </div>
+
+      <div className='navMenu'>
+        <NavbarMenu />
       </div>
+      {show ? <NavbarMenu /> : null}
+      <button className='menuBtn' onClick={handleShowMenu}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className='mx-2'>
+          <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
+        </svg>
+      </button>
     </nav>
   )
 }
---
+++ src/components/NavbarMenu/NavbarMenu.jsx
@@ -0,0 +1,26 @@
+import NavbarLinks from '../NavbarLinks/NavbarLinks';
+import './NavbarMenu.css'
+
+const NavbarMenu = () => {
+
+    const links = [
+        { key: 1, title: 'Home', to: '/home' },
+        { key: 2, title: 'Cities', to: '/cities' },
+        { key: 3, title: 'Stays', to: '/stays' },
+        { key: 4, title: 'Car rentals', to: '/car-rentals' }
+    ]
+
+    return (
+        <div className='fullMenu'>
+            <div className='links text-white'>
+                {links.map((link) => (<NavbarLinks key={link.key} title={link.title} to={link.to} />))}
+            </div>
+            <div className="user">
+                <button className=" bg-gradient-to-r from-purple-500 bg-purple-400 mx-2 px-3 py-1 text-sm shadow-sm font-medium tracking-wider  text-white rounded-full hover:shadow-2xl hover:bg-purple-700 hover:from-purple-700">Login</button>
+                <img className='h-8 mx-2 aspect-square' src="https://i.postimg.cc/9MLsQhpR/blank-avatar.png" alt="blank avatar" />
+            </div>
+        </div>
+    )
+}
+
+export default NavbarMenu
