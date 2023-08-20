4a7898f2c91a7a2a8aecc917ffc11a3824d8784c
arielLevita <levita.ariel@gmail.com>
2023-08-20 20:08:01 -0300

    Responsive footer

--- src/components/Destinations/Destinations.jsx
+++ src/components/Destinations/Destinations.jsx
@@ -3,7 +3,7 @@ import Carousel from '../Carousel/Carousel';
 const Destinations = () => {
     return (
         <section className='p-3' id='destinations'>
-            <h2 className='mt-5 text-center text-4xl font-semibold'>Popular Mytineraries</h2>
+            <h2 className='mt-5 text-center text-2xl md:text-4xl font-semibold'>Popular Mytineraries</h2>
             <p className='text-center font-semibold text-purple-700 mb-6'>Find you perfect destination</p>
             <Carousel />
         </section>
