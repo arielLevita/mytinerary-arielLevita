874964e50a85874ce26060ada4c0ec3382346151
arielLevita <levita.ariel@gmail.com>
2023-12-16 17:24:31 -0300

    fixing prtectedroute error 3

--- src/router/ProtectedRoute.jsx
+++ src/router/ProtectedRoute.jsx
@@ -1,20 +1,33 @@
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-
 /**
  * The ProtectedRoute component checks if there is a user logged in and either renders the children or
  * navigates back to the previous page.
  * @returns If the `user` is not defined, the `children` component is returned. Otherwise, the
  * `navigate(-1)` function is called.
  */
-const ProtectedRoute = ({children}) => {
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+/* const ProtectedRoute = ({children}) => {
     let user = useSelector(store => store.userReducer.user);
     const navigate = useNavigate();
 
     if(!user) {
         return children
     }
-    return navigate("/cities")
+    return navigate(-1)
 }
 
-export default ProtectedRoute
+export default ProtectedRoute */
+
+const ProtectedRoute = ({ Outlet, redirectTo = -1 }) => {
+    let user = useSelector(store => store.userReducer.user);
+    const navigate = useNavigate();
+
+    if (!user) {
+        return <Outlet />;
+    }
+
+    return navigate(redirectTo);
+};
+
+export default ProtectedRoute;
